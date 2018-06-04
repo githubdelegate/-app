c1cbc6553cb33283d415e62ab3f59fdde762f1c3
zy <ostmail@163.com>
2018-06-04 13:48:20 +0800

    init project

--- 定位/ViewController.m
+++ 定位/ViewController.m
@@ -7,23 +7,63 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
-
+@interface ViewController ()<CLLocationManagerDelegate>
+@property (nonatomic,strong) CLLocationManager *clMgr;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    CLLocationManager *clMgr = [[CLLocationManager alloc] init];
+    self.clMgr = clMgr;
+    self.clMgr.delegate = self;
+    self.clMgr.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+    self.clMgr.activityType = CLActivityTypeFitness;
+    
+    if ([CLLocationManager authorizationStatus] != kCLAuthorizationStatusAuthorizedAlways) {
+        [clMgr requestAlwaysAuthorization];
+    }
+    
+    // 开始
+    clMgr.pausesLocationUpdatesAutomatically = YES;
+    [clMgr startMonitoringVisits];
+    [clMgr startMonitoringSignificantLocationChanges];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)locationManager:(CLLocationManager *)manager didVisit:(CLVisit *)visit{
+    
 }
 
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations{
+    CLLocation *lastLocation = [locations lastObject];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+    if (error.code == kCLAuthorizationStatusDenied) {
+        [self.clMgr stopMonitoringVisits];
+        
+        NSLog(@" 没有授权 ,请授权");
+        return;
+    }
+}
+
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status{
+    if (status != kCLAuthorizationStatusAuthorizedAlways) {
+        // 不是一直定位,提示用户 需要开启一直定位
+    }
+    
+    // 开启一直定位
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+     didUpdateLocations:(NSArray<CLLocation *> *)locations{
+    
+}
 
 @end
